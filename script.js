02fb09bdcd169ac4f026bc3b95133857c5f81bc3
smellslikenirvana <nmitalinanda@gmail.com>
2026-01-06 03:12:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,415 @@
+
+const MEMORY_DATA = {
+  delhi: {
+    title: "Delhi",
+    photos: [
+      { src: "del1.jpg", caption: "First picture together UwU \nI was lowkey so nervous being high around you, I was tryna not freak out so hard T_T But now I only wanna be high if it's around you." },
+      { src: "del2.jpg", caption: "My first actual picture of you.\nKiona. Spontaneous date. Fun lamps but couldn't read shit. I just wanted a picture of you so bad. Having a crush on a man who doesn't post is such a struggle." },
+      { src: "del3.jpg", caption: "Iconic picture. Got two bouqets from you ^-^ sad we left those roses in the auto tho \n Celebrating my UBC admit-that purple rice beer, sitting outside Blue Tokai chugging, Rock night at Piano Man, stoner guy who grew his own stuff. A classic        samarth-mitali date!" },
+      { src: "del4.jpg", caption: "Kaleshi night at Gurgaon BYOB courtesy of Geetansh's DG win" },
+      { src: "del5.jpg", caption: "This was so random. Was this our first clear couple picture?" },
+      { src: "del6.jpg", caption: "Ayyy Kailash House Day. \nThis twinky ass man bullying you to roll correctly." },
+      { src: "del7.jpg", caption: "Priyansh ka approval milne ke baad:" },
+      { src: "del8.jpg", caption: "Ayy host ka approval bhi mil gaya. Ojas I have no clue sry T_T \nRutuja ka room for smoking up was peak. Esp Ravinder coming in at the end telling us he pushed through the warden. We were so close to getting caught ngl" },
+      { src: "del9.jpg", caption: "Man drinking Strawb matcha boba tea while posing to work. \n(Actually you were probably looking at Convenor vote count now that I think of it)" },
+      { src: "del10.jpg", caption: "Batch Photoshoot Day<333 \nThis is the only pic my mom has of you lmao" },
+      { src: "del11.jpg", caption: "Cf hard launch. Kaafizyada. \n\nSide note: loved the way you were walking behind me helping with the saree." },
+      { src: "del12.jpg", caption: "Post batch photoshoot celebration *wink wink*" },
+      { src: "del13.jpg", caption: "COL202 major study sesh ft. your first (apology) gift to me" },
+      { src: "del14.jpg", caption: "Post Director's Dinner. \nThose were the steps where we first smoked together (and then again with Rutuja)" },
+      { src: "del15.jpg", caption: "Dogra Rooftop witnessed A LOT" },
+      { src: "del16.jpg", caption: "[POST PUNE]: Bechaare bacche ko Burrata Pizza nahi khane diya :((( " },
+      { src: "del17.jpg", caption: "I blame it on your loveee~~~" },
+      { src: "del18.jpg", caption: "32nd avenue ka picture booth making us realize how chopped we look ft. sarojini ki mat." },
+      { src: "del19.jpg", caption: "Cute guy handling his crying-for-no-reason mascara-running-all-over bhand girlfriend. \nSpilled one coffee and gave the other one away. (my) First Kenangan visit L." },
+      { src: "del20.jpg", caption: "Convocation Day<3 You met my mom LMAO (and she almost caught us too)" },
+      { src: "del21.jpg", caption: "Man spoilt me sm with those flowers hehe" },
+      { src: "del22.jpg", caption: "My early 22nd birthday party. Our first house party together. You're so cute." },
+      { src: "del23.jpg", caption: "Finally got to see Aerocity at night. This place had the mushroom soup you loved sm." },
+      { src: "del24.jpg", caption: "Cutiesss" },
+      { src: "del25.jpg", caption: "Cutiesss pt.2" },
+      { src: "del26.jpg", caption: "Waiting for our cab sitting in front of Novotel(next) after having a great night at Aerocity." },
+      { src: "del27.jpg", caption: "Our last night together pre-LDR :(\nSo worth it despite how tired we were finding a place. Love how we match each other's energy." },
+      { src: "del28.jpg", caption: "Our airport pics were so cute. \n\nPS: Sutta at the airport is a staple? T_T" },
+      { src: "del29.jpg", caption: "I could kiss you forever" },
+      { src: "del30.jpg", caption: "Not letting you get out of my hug ever again" }
+    ]
+  },
+
+  jaipur: {
+    title: "Jaipur",
+    photos: [
+      { src: "jpr1.jpg", caption: "Tapriiii \nWas gonna soft launch you to this but man said his arms looked too skinny :/" },
+      { src: "jpr2.jpg", caption: "That idk-what-to-do-with-my-hands pose of yours \nYou look so fine even after a sleepless night." },
+      { src: "jpr3.jpg", caption: "Passenger princessing UwU" },
+      { src: "jpr4.jpg", caption: "ik you see this everyday at home but I had to take the quintessential jaipur hawa mahal pic " },
+      { src: "jpr5.jpg", caption: "Packed conservative only to find out your family is chill like that." }
+    ]
+  },
+
+  pune: {
+    title: "Pune",
+    photos: [
+      { src: "pune1.jpg", caption: "Pune airport reunion after what felt like the longest month ever" },
+      { src: "pune2.jpg", caption: "Our first cafe date in Pune<33 I slept for like 1.5 days straight after reaching there lmao. In my defence I sleep so well next to you" },
+      { src: "pune3.jpg", caption: "idc who Sumukh Bansal is, my boyfriend is adorable!!" },
+      { src: "pune4.jpg", caption: "Cutiesss (Pune version)" },
+      { src: "pune5.jpg", caption: "Struggle to take a couple picture is so real" },
+      { src: "pune6.jpg", caption: "FC Road tour hype ^-^" },
+      { src: "pune7.jpg", caption: "Boyfriend tshirt flex" },
+      { src: "pune8.jpg", caption: "We were trying so hard to take pictures in that mirror T_T" },
+      { src: "pune9.jpg", caption: "Polaroid<333 seriously how do we manage to have so much fun in a single night" },
+      { src: "pune10.jpg", caption: "HARD LAUNCH FT EK DIN MARNA HAI BY DARZI" },
+      { src: "pune11.jpg", caption: "Corporate banda taking a half day to have chai and bun maska with his gf" },
+      { src: "pune12.jpg", caption: "We looked so chopped in the pics so here's the pesto pasta you were craving that day \nHighlight of the day: Balsamic Vinegar" },
+      { src: "pune13.jpg", caption: "BHS and Tanish ki khatirdari" },
+      { src: "pune14.jpg", caption: "Compliance training" },
+      { src: "pune15.jpg", caption: "Man is obsessed with posing as if he's working (My tech nerd bf was Leetcoding)" },
+      { src: "pune16.jpg", caption: "Lowkey fire sutta shot, had to include it." },
+      { src: "pune17.jpg", caption: "The cat meme (pls tell me you remem what im talking about)" },
+      { src: "pune18.jpg", caption: "I'd like to think I was responsible for you laughing this hard (IT BETTER NOT BE YOU LAUGHING AT YOUR OWN JOKE)" },
+      { src: "pune19.jpg", caption: "Cut up some mangoes-housewifey shit \nP.S.: WHich ones did we end up buying? Ratnagiri?" },
+      { src: "pune20.jpg", caption: "Fell off scooty T_T Hospital date checked off our relationship bucket list (I can still picture the nurse laughing at my hickeys asking if I got hurt there too T_T) RIP your fav pants tho" },
+      { src: "pune21.jpg", caption: "Nips \nFeeding me some pakodas to make up for injuring me on the scooty " },
+      { src: "pune22.jpg", caption: "Genuinely love you so much for being so kind and patient after the most exhausting day \nGot a tetanus shot together- peak romance fr" },
+      { src: "pune23.jpg", caption: "Caught sneaking a cuddle pic" },
+      { src: "pune24.jpg", caption: "Banda doing housewifey shit" },
+      { src: "pune25.jpg", caption: "Mf cubed up mangoes better than me" },
+      { src: "pune26.jpg", caption: "Your expression while kissing me>>>" },
+      { src: "pune27.jpg", caption: "I look so happyyy" },
+      { src: "pune28.jpg", caption: "This pic has no business going so hard \nYour pouch is safe with me<3" },
+      { src: "pune29.jpg", caption: "LDR again post-Pune :( Feet revealll" }
+    ]
+  },
+
+  hyderabad: {
+    title: "Hyd",
+    photos: [
+      { src: "comingsoon.png", caption: "Coming soon...<333" },
+    ]
+  }
+};
+
+
+function drawPixelatedImage({
+  canvas,
+  imageSrc,
+  pixelScale,
+  maxWidth = null,
+  maxHeight = null,
+}) {
+  const ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
+
+  const img = new Image();
+  img.src = imageSrc;
+
+  img.onload = () => {
+    let scale = 1;
+
+    if (maxWidth && maxHeight) {
+      scale = Math.min(
+        maxWidth / img.width,
+        maxHeight / img.height
+      );
+    }
+
+    canvas.width = img.width * scale;
+    canvas.height = img.height * scale;
+
+    const tempCanvas = document.createElement("canvas");
+    const tempCtx = tempCanvas.getContext("2d");
+    tempCtx.imageSmoothingEnabled = false;
+
+    const smallW = img.width / pixelScale;
+    const smallH = img.height / pixelScale;
+
+    tempCanvas.width = smallW;
+    tempCanvas.height = smallH;
+
+    tempCtx.drawImage(img, 0, 0, smallW, smallH);
+    ctx.drawImage(
+      tempCanvas,
+      0, 0, smallW, smallH,
+      0, 0, canvas.width, canvas.height
+    );
+  };
+}
+
+drawPixelatedImage({
+  canvas: document.getElementById("mapCanvas"),
+  imageSrc: "Group 1.png",
+  pixelScale: 2.3,
+  maxWidth: window.innerWidth,
+  maxHeight: window.innerHeight,
+});
+
+function drawPixelatedSprite(canvasId, imageSrc, pixelScale) {
+  const canvas = document.getElementById(canvasId);
+  const ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
+
+  const img = new Image();
+  img.src = imageSrc;
+
+  img.onload = () => {
+    const w = canvas.width;
+    const h = canvas.height;
+
+    const temp = document.createElement("canvas");
+    const tctx = temp.getContext("2d");
+    tctx.imageSmoothingEnabled = false;
+
+    const smallW = Math.floor(w / pixelScale);
+    const smallH = Math.floor(h / pixelScale);
+
+    temp.width = smallW;
+    temp.height = smallH;
+
+    tctx.drawImage(img, 0, 0, smallW, smallH);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.drawImage(temp, 0, 0, smallW, smallH, 0, 0, w, h);
+  };
+}
+
+
+function pixelatePin(canvas, imgSrc, scale = 4) {
+  const ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
+
+  const img = new Image();
+  img.src = imgSrc;
+
+  img.onload = () => {
+    const w = canvas.width;
+    const h = canvas.height;
+
+    const smallW = Math.floor(w / scale);
+    const smallH = Math.floor(h / scale);
+
+    // draw tiny
+    ctx.clearRect(0, 0, w, h);
+    ctx.drawImage(img, 0, 0, smallW, smallH);
+
+    // scale up
+    ctx.drawImage(
+      canvas,
+      0, 0, smallW, smallH,
+      0, 0, w, h
+    );
+  };
+}
+
+drawPixelatedSprite(
+  "closed-envelope-canvas",
+  "envelope.png",
+  3.5
+);
+
+drawPixelatedSprite(
+  "open-envelope-canvas",
+  "open_envelope.png",
+  3.5
+);
+
+// const envelope = document.getElementById("envelope");
+const envelope = document.getElementById("closed-envelope-canvas");
+const sceneEnvelope = document.getElementById("scene-envelope");
+const sceneLetter = document.getElementById("scene-letter");
+
+// envelope.addEventListener("click", () => {
+//   envelope.classList.add("open");
+
+//   // setTimeout(() => {
+//   //   sceneEnvelope.classList.remove("active");
+//   //   sceneEnvelope.classList.add("hidden");
+
+//   //   sceneLetter.classList.remove("hidden");
+//   //   sceneLetter.classList.add("active");
+//   // }, 500);
+//   setTimeout(() => {
+//     sceneEnvelope.classList.remove("active");
+//     sceneEnvelope.classList.add("hidden");
+
+//     sceneLetter.classList.remove("hidden");
+//     sceneLetter.classList.add("active");
+//   }, 400);
+// });
+
+envelope.addEventListener("click", () => {
+  envelope.classList.add("open");
+
+  setTimeout(() => {
+    sceneEnvelope.classList.remove("active");
+    sceneEnvelope.classList.add("hidden");
+
+    sceneLetter.classList.remove("hidden");
+    sceneLetter.classList.add("active");
+
+    autoFadeToMap();
+  }, 400); // let envelope opening play
+});
+
+// function autoFadeToMap() {
+//   setTimeout(() => {
+//     sceneLetter.classList.remove("active");
+//     sceneLetter.classList.add("hidden");
+
+//     // sceneMap.classList.remove("hidden");
+//     // sceneMap.classList.add("active");
+
+//     document.body.classList.add("show-map");
+//   }, 3000);
+// }
+
+function autoFadeToMap() {
+  setTimeout(() => {
+    sceneLetter.classList.add("fade-out");
+
+    setTimeout(() => {
+      sceneLetter.classList.remove("active", "fade-out");
+      sceneLetter.classList.add("hidden");
+
+      // sceneMap.classList.remove("hidden");
+      // sceneMap.classList.add("active");
+
+      document.body.classList.add("show-map");
+    }, 1000);
+  }, 1500); // This was 3000 originally
+}
+
+
+
+// const sceneMap = document.getElementById("scene-map");
+
+// // setTimeout(() => {
+// //   sceneLetter.classList.remove("active");
+// //   sceneLetter.classList.add("hidden");
+
+// //   sceneMap.classList.remove("hidden");
+// //   sceneMap.classList.add("active");
+// // }, 3500);
+
+// sceneLetter.addEventListener("click", () => {
+//   sceneLetter.classList.remove("active");
+//   sceneLetter.classList.add("hidden");
+
+//   sceneMap.classList.remove("hidden");
+//   sceneMap.classList.add("active");
+
+//   document.body.classList.add("show-map");
+// });
+
+
+document.querySelectorAll(".pin-canvas").forEach(pin => {
+  pixelatePin(pin, "Map pin.png", 1.15);
+});
+
+const modal = document.getElementById("memory-modal");
+const mainPhoto = document.getElementById("main-photo");
+const captionBox = document.getElementById("photo-caption");
+const thumbnailStrip = document.getElementById("thumbnail-strip");
+const title = document.getElementById("location-title");
+
+
+let currentMemory = null;
+let currentIndex = 0;
+
+function openMemory(locationKey) {
+  console.log("openMemory called with:", locationKey);
+  currentMemory = MEMORY_DATA[locationKey];
+  currentIndex = 0;
+
+  document.body.classList.add("modal-open");
+
+  title.innerHTML = `
+  <img src="red-heart.png" style="width: 8%; height: 50%;" class="title-heart" alt="heart" />
+  ${currentMemory.title}
+  <img src="red-heart.png" style="width: 8%; height: 50%;" class="title-heart" alt="heart" />
+`;
+
+
+  buildThumbnails();
+  showPhoto(0);
+
+  modal.classList.remove("hidden");
+}
+
+
+function buildThumbnails() {
+  thumbnailStrip.innerHTML = "";
+
+  currentMemory.photos.forEach((photo, index) => {
+    const img = document.createElement("img");
+    img.src = photo.src;
+
+    img.onclick = () => showPhoto(index);
+
+    thumbnailStrip.appendChild(img);
+  });
+}
+
+function showPhoto(index) {
+  currentIndex = index;
+
+  mainPhoto.src = currentMemory.photos[index].src;
+  captionBox.textContent = currentMemory.photos[index].caption;
+
+  [...thumbnailStrip.children].forEach((img, i) => {
+    img.classList.toggle("active", i === index);
+  });
+
+  document.getElementById("prev-btn").disabled = index === 0;
+  document.getElementById("next-btn").disabled =
+    index === currentMemory.photos.length - 1;
+}
+
+
+document.querySelectorAll(".pin-wrapper").forEach(pin => {
+  pin.addEventListener("click", () => {
+    console.log("PIN CLICKED", pin.dataset.memory);
+    const key = pin.dataset.memory;
+    openMemory(key);
+  });
+});
+
+document.getElementById("next-btn").onclick = () => {
+  if (currentIndex < currentMemory.photos.length - 1) {
+    showPhoto(currentIndex + 1);
+  }
+};
+
+document.getElementById("prev-btn").onclick = () => {
+  if (currentIndex > 0) {
+    showPhoto(currentIndex - 1);
+  }
+};
+
+
+document.getElementById("close-modal").onclick = () => {
+  modal.classList.add("hidden");
+  document.body.classList.remove("modal-open");
+};
+
+document.addEventListener("keydown", (e) => {
+  if (modal.classList.contains("hidden")) return; // modal not open
+
+  switch (e.key) {
+    case "ArrowRight":
+      if (currentIndex < currentMemory.photos.length - 1) {
+        showPhoto(currentIndex + 1);
+      }
+      break;
+
+    case "ArrowLeft":
+      if (currentIndex > 0) {
+        showPhoto(currentIndex - 1);
+      }
+      break;
+
+    case "Escape":
+      modal.classList.add("hidden");
+      break;
+  }
+});
